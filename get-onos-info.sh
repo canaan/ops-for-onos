53333b05c30c588ca9b41e31cdefdc3fff38a77b
Yoshihko Kanaumi <yoshihiko@gmail.com>
2015-11-19 11:49:52 +0900

    A first commit of ops-for-onos

---
+++ get-onos-info.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+INFOLOG_DIR="/home/sdn/logs/onos-info"
+
+roles() {
+        for i in $OC1 $OC5 $OC6
+        do
+                client -h $i -u karaf "roles" > $INFOLOG_DIR/"$i"_roles_$(date +%F_%T)
+        done
+}
+
+devices() {
+        for i in $OC1 $OC5 $OC6
+        do
+                client -h $i -u karaf "devices" > $INFOLOG_DIR/"$i"_devices_$(date +%F_%T)
+        done
+}
+
+master() {
+	for i in $OC1 $OC5 $OC6
+	do
+        	client -h $i -u karaf "master" > $INFOLOG_DIR/"$i"_master_$(date +%F_%T)
+	done
+}
+
+case "$1" in
+roles)
+	set -x
+	roles
+	;;
+devices)
+	set -x
+	devices
+	;;
+master)
+	set -x
+	master
+	;;
+*)
+	echo "usage:30 {master|roles|devices}"
+esac
