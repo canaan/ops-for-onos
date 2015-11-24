334b1fc3987aaf382bf850429827c29b5673e653
Yoshihko Kanaumi <yoshihiko@gmail.com>
2015-11-24 12:41:53 +0900

    Add the command-line option (all argu)

--- get-onos-info.sh
+++ get-onos-info.sh
@@ -2,6 +2,32 @@
 
 INFOLOG_DIR="/home/sdn/logs/onos-info"
 
+all() {
+        for i in $OC1 $OC5 $OC6
+        do
+                client -h $i -u karaf "summary" > $INFOLOG_DIR/"$i"_summary_$(date +%F_%T)
+                client -h $i -u karaf "links" > $INFOLOG_DIR/"$i"_links_$(date +%F_%T)
+                client -h $i -u karaf "roles" > $INFOLOG_DIR/"$i"_roles_$(date +%F_%T)
+                client -h $i -u karaf "devices" > $INFOLOG_DIR/"$i"_devices_$(date +%F_%T)
+                client -h $i -u karaf "masters" > $INFOLOG_DIR/"$i"_masters_$(date +%F_%T)
+        done
+
+}
+
+summary() {
+	for i in $OC1 $OC5 $OC6
+	do
+                client -h $i -u karaf "summary" > $INFOLOG_DIR/"$i"_summary_$(date +%F_%T)
+	done
+}
+
+links() {
+	for i in $OC1 $OC5 $OC6
+	do
+                client -h $i -u karaf "links" > $INFOLOG_DIR/"$i"_links_$(date +%F_%T)
+	done
+}
+
 roles() {
         for i in $OC1 $OC5 $OC6
         do
@@ -58,6 +84,18 @@ info)
 	set -x
 	info
 	;;
+summary)
+	set -x
+	summary
+	;;
+links)
+	set -x
+	links
+	;;
+all)
+	set -x
+	all
+	;;
 *)
-	echo "usage:30 {masters|roles|devices|trace|info}"
+	echo "usage:30 {masters|roles|devices|trace|info|summary|lnks|all}"
 esac
