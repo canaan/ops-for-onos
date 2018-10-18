d59270a98f3fd661f5a97d2830cbb6723ab3088d
Yoshihiko Kanaumi <y-kanaumi@jgn-x.jp>
2018-10-18 14:57:11 +0900

    adding the script files for ONOS operation

---
+++ onos-push-keys.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+all() {
+        for i in $OC1 $OC2 $OC3 $OC4 $OC5 $OC6
+        do
+                onos-push-keys $i
+        done
+}
+
+case "$1" in
+all)
+        set -x
+        all
+        ;;
+*)
+        echo "usage:30 {all}"
+esac
